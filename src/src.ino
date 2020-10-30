b26f8b31a03e5bb24cb49c52f31ee49a67c3b0c8
Yey007 <yey007tech@gmail.com>
2020-10-30 17:31:50 -0400

    beginning code I stole from arduino's website

---
+++ src/src.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+
+char ssid[] = "network";
+char pass[] = "password";
+int keyIndex = 0;
+
+int status = WL_IDLE_STATUS;
+char server[] = "www.google.com";
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+  
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+
+  Serial.println("Connected to wifi");
+  Serial.println("\nStarting connection to server...");
+}
+
+void loop() {
+  
+}
